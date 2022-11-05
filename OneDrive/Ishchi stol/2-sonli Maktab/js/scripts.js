5a14c0e7400b99c73b86db3e9a749b352fa297da
javlonbek16 <nemaotovjavlonbek16@gmail.com>
2022-11-05 21:58:29 +0500

    first commit

---
+++ OneDrive/Ishchi stol/2-sonli Maktab/js/scripts.js
@@ -0,0 +1,8 @@
+// https: startbootstrap.com / template - overviews / modern - business;
+
+// https: github.com / StartBootstrap / startbootstrap -
+//   modern -
+//   business / blob / master / LICENSE;
+
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
